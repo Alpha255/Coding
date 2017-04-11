e0f012c513d2b786d2071e450c4c82b1b58e86ca
maxiao <maxiao1925@virtuos.com.cn>
2017-04-11 18:49:10 +0800

    *. (WIP)Refactor and Transplant... (For D3D11 Demos)...

--- D3D11Basic/D3DUtil/D3DGraphic.cpp
+++ D3D11Basic/D3DUtil/D3DGraphic.cpp
@@ -1,3 +1,230 @@
 #include "D3DGraphic.h"
+#include <fstream>
 
 D3DGraphic* D3DGraphic::m_sInstance = nullptr;
+
+D3DGraphic::D3DGraphic()
+{
+	memset(&m_D3DPipeLineState, 0, sizeof(D3DPipelineState));
+	memset(&m_FlushState, 0, sizeof(bool) * eFSCount);
+	memset(&m_SwapChainDesc, 0, sizeof(DXGI_SWAP_CHAIN_DESC));
+}
+
+void D3DGraphic::CreateTexture2D(ID3D11Texture2D** ppTexture, DXGI_FORMAT fmt, uint32_t width, uint32_t height,
+	uint32_t bindFlags, uint32_t mipLevels, uint32_t arraySize, uint32_t cpuFlags, uint32_t sampleCount, uint32_t sampleQuality,
+	uint32_t miscFlags, D3D11_USAGE usage)
+{
+	D3D11_TEXTURE2D_DESC texDesc;
+	memset(&texDesc, 0, sizeof(D3D11_TEXTURE2D_DESC));
+	texDesc.Format = fmt;
+	texDesc.Width = width;
+	texDesc.Height = height;
+	texDesc.BindFlags = bindFlags;
+	texDesc.MipLevels = mipLevels;
+	texDesc.ArraySize = arraySize;
+	texDesc.CPUAccessFlags = cpuFlags;
+	texDesc.SampleDesc.Count = sampleCount;
+	texDesc.SampleDesc.Quality = sampleQuality;
+	texDesc.MiscFlags = miscFlags;
+	texDesc.Usage = usage;
+
+	HRCheck(m_D3DDevice->CreateTexture2D(&texDesc, nullptr, ppTexture));
+}
+
+void D3DGraphic::CreateDepthStencilView(ID3D11DepthStencilView** ppDSV, DXGI_FORMAT fmt, uint32_t width, uint32_t height)
+{
+	assert(ppDSV);
+	assert(width > 0U);
+	assert(height > 0U);
+
+	ID3D11Texture2D* pTexture = nullptr;
+	CreateTexture2D(&pTexture, fmt, width, height, D3D11_BIND_DEPTH_STENCIL, 1U, 1U, 0U, 0U, 0U, 0U, D3D11_USAGE_DEFAULT);
+	HRCheck(m_D3DDevice->CreateDepthStencilView(pTexture, nullptr, ppDSV));
+	SafeRelease(pTexture);
+}
+
+void D3DGraphic::InitD3DEnvironment(HWND hWnd, uint32_t width, uint32_t height, bool bWindowed)
+{
+	assert(hWnd);
+
+	/// Create D3D Device and SwapChain
+	uint32_t flags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+	flags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0
+	};
+
+	m_SwapChainDesc.BufferCount = eCTBackBuffer;
+	m_SwapChainDesc.BufferDesc.Width = width;
+	m_SwapChainDesc.BufferDesc.Height = height;
+	m_SwapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	m_SwapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+	m_SwapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	m_SwapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	m_SwapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_STRETCHED;
+	m_SwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_BACK_BUFFER;
+	m_SwapChainDesc.OutputWindow = hWnd;
+	m_SwapChainDesc.Windowed = bWindowed;
+	m_SwapChainDesc.SampleDesc.Count = 1;
+	m_SwapChainDesc.SampleDesc.Quality = 0;
+	m_SwapChainDesc.SwapEffect = ((eCTBackBuffer > 1) ? DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL : DXGI_SWAP_EFFECT_DISCARD);
+	m_SwapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	for (UINT index = 0; index < ARRAYSIZE(driverTypes); ++index)
+	{
+		if (SUCCEEDED(D3D11CreateDeviceAndSwapChain(nullptr, 
+			driverTypes[index], 
+			nullptr, 
+			flags,
+			featureLevels, 
+			ARRAYSIZE(featureLevels), 
+			D3D11_SDK_VERSION,
+			&m_SwapChainDesc, 
+			m_SwapChain.GetResource(), 
+			m_D3DDevice.GetResource(),
+			nullptr, 
+			m_D3DContext.GetResource())))
+		{
+			/// Create Back Buffer 
+			RecreateBackBuffer();
+
+			/// Create Depth Stencil View
+			CreateDepthStencilView(m_DefaultDepthStencilView.GetResource(), DXGI_FORMAT_D24_UNORM_S8_UINT, width, height);
+
+			break;
+		}
+	}
+
+	assert(!"Failed to create D3D environment!!!");
+}
+
+void D3DGraphic::CreateShaderResourceView(ID3D11ShaderResourceView** ppSRV, char* pFileName)
+{
+	assert(ppSRV);
+	assert(pFileName);
+}
+
+void D3DGraphic::CreateStreamBuffer(ID3D11Buffer** ppBuffer, D3D11_BIND_FLAG bindFlag, uint32_t byteWidth, 
+	D3D11_USAGE usage, const void* pBuf, uint32_t cpuAccessFlag)
+{
+	assert(ppBuffer);
+
+	switch (bindFlag)
+	{
+	case D3D11_BIND_INDEX_BUFFER:
+	case D3D11_BIND_VERTEX_BUFFER:
+	{
+		D3D11_BUFFER_DESC bufDesc;
+		D3D11_SUBRESOURCE_DATA srcData;
+		memset(&bufDesc, 0, sizeof(D3D11_BUFFER_DESC));
+		memset(&srcData, 0, sizeof(D3D11_SUBRESOURCE_DATA));
+
+		bufDesc.ByteWidth = byteWidth;
+		bufDesc.Usage = usage;
+		bufDesc.BindFlags = bindFlag;
+		bufDesc.CPUAccessFlags = cpuAccessFlag;
+		bufDesc.MiscFlags = 0U;
+		bufDesc.StructureByteStride = 0U;
+
+		srcData.pSysMem = pBuf;
+		srcData.SysMemPitch = 0U;
+		srcData.SysMemSlicePitch = 0U;
+
+		HRCheck(m_D3DDevice->CreateBuffer(&bufDesc, ((nullptr == pBuf) ? nullptr : &srcData), ppBuffer));
+	}
+		break;
+	case D3D11_BIND_CONSTANT_BUFFER:
+	{
+		D3D11_BUFFER_DESC bufDesc;
+		memset(&bufDesc, 0, sizeof(D3D11_BUFFER_DESC));
+
+		bufDesc.Usage = usage;
+		bufDesc.ByteWidth = byteWidth;
+		bufDesc.CPUAccessFlags = cpuAccessFlag;
+		bufDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+		bufDesc.MiscFlags = 0U;
+		bufDesc.StructureByteStride = 0U;
+
+		HRCheck(m_D3DDevice->CreateBuffer(&bufDesc, nullptr, ppBuffer));
+	}
+		break;
+	default:
+		assert(!"Unsupport yet!!!");
+		break;
+	}
+}
+
+void D3DGraphic::CreateBlendState(ID3D11BlendState** ppBlendState, D3D11_BLEND_DESC* pBlendDesc)
+{
+	assert(ppBlendState);
+
+	HRCheck(m_D3DDevice->CreateBlendState(pBlendDesc, ppBlendState));
+}
+
+void D3DGraphic::CompileShaderFile(ID3DBlob** ppRes, char* pFileName, char* pEntryPoint, char* pTarget)
+{
+	assert(ppRes && pFileName && pEntryPoint && pTarget);
+
+	std::ifstream file(pFileName, std::ios::in);
+	if (file.good())
+	{
+		file.seekg(std::ios::end);
+		size_t fileSize = file.tellg();
+
+		char* pData = new char[fileSize]();
+		file.seekg(std::ios::beg);
+		file.read(pData, fileSize);
+
+#if defined(DEBUG) || defined(_DEBUG)
+		uint32_t flags = D3DCOMPILE_ENABLE_STRICTNESS | D3DCOMPILE_DEBUG;
+#else
+		uint32_t flags = D3DCOMPILE_ENABLE_STRICTNESS;
+#endif
+
+		ID3DBlob* pErrMsg = nullptr;
+		if (FAILED(D3DCompile(pData, fileSize, pFileName, nullptr, nullptr, pEntryPoint, pTarget, flags, 0U, ppRes, &pErrMsg)))
+		{
+			OutputDebugStringA((char*)pErrMsg->GetBufferPointer());
+			assert(!"Shader compile failed!!!");
+		}
+
+		SafeRelease(pErrMsg);
+		SafeDeleteArray(pData);
+		file.close();
+	}
+
+	assert(!"Failed to open specified shader file!!!");
+}
+
+void D3DGraphic::CreateVertexShader(ID3D11VertexShader** ppVS, char* pFileName, char* pEntryPoint)
+{
+	assert(ppVS);
+
+	ID3DBlob* pRes = nullptr;
+	CompileShaderFile(&pRes, pFileName, pEntryPoint, "vs_5_0");
+	HRCheck(m_D3DDevice->CreateVertexShader(pRes->GetBufferPointer(), pRes->GetBufferSize(), nullptr, ppVS));
+	SafeRelease(pRes);
+}
+
+void D3DGraphic::CreatePixelShader(ID3D11PixelShader** ppPS, char* pFileName, char* pEntryPoint)
+{
+	assert(ppPS);
+
+	ID3DBlob* pRes = nullptr;
+	CompileShaderFile(&pRes, pFileName, pEntryPoint, "ps_5_0");
+	HRCheck(m_D3DDevice->CreatePixelShader(pRes->GetBufferPointer(), pRes->GetBufferSize(), nullptr, ppPS));
+	SafeRelease(pRes);
+}
--- D3D11Basic/D3DUtil/D3DGraphic.h
+++ D3D11Basic/D3DUtil/D3DGraphic.h
@@ -8,28 +8,108 @@
 class D3DGraphic
 {
 public:
+	enum eD3DCounter
+	{
+		eCTVertexStream = 1,
+		eCTRenderTarget = 1,
+		eCTBackBuffer = 2,
+	};
+
+	enum eD3DFlushState
+	{
+		eFSRenderTarget,
+		eFSDepthStencil,
+		eFSViewport,
+		eFSRasterizerState,
+		eFSBlendState,
+		eFSDepthStencilState,
+		eFSCount
+	};
+
+	enum eD3DResourceType
+	{
+		eRTTexture,
+		eRTShader
+	};
+
 	static void CreateInstance(void) { if (nullptr == m_sInstance) { m_sInstance = new D3DGraphic(); assert(m_sInstance); } }
 	static D3DGraphic* GetInstance(void) { return m_sInstance; }
 	static void DestoryInstance(void) { SafeDelete(m_sInstance); }
 
-	void InitD3DDevice(HWND hWnd, uint32_t width, uint32_t height, bool bWindowed);
+	void InitD3DEnvironment(HWND hWnd, uint32_t width, uint32_t height, bool bWindowed);
+
+	void CreateDepthStencilView(__out ID3D11DepthStencilView** ppDSV, DXGI_FORMAT fmt, uint32_t width, uint32_t height);
+
+	void CreateTexture2D(__out ID3D11Texture2D** ppTexture, DXGI_FORMAT fmt, uint32_t width, uint32_t height,
+		uint32_t bindFlags, uint32_t mipLevels = 1U, uint32_t arraySize = 1U, uint32_t cpuFlags = 0U, uint32_t sampleCount = 0U, uint32_t sampleQuality = 0U,
+		uint32_t miscFlags = 0U, D3D11_USAGE usage = D3D11_USAGE_DEFAULT);
+
+	void CreateShaderResourceView(__out ID3D11ShaderResourceView** ppSRV, char* pFileName);
+
+	void CreateStreamBuffer(__out ID3D11Buffer** ppBuffer, D3D11_BIND_FLAG bindFlag, uint32_t byteWidth, D3D11_USAGE usage, const void* pBuf, uint32_t cpuAccessFlag = 0U);
+
+	void CreateBlendState(__out ID3D11BlendState** ppBlendState, D3D11_BLEND_DESC* pBlendDesc);
+
+	void CreateVertexShader(__out ID3D11VertexShader** ppVS, char* pFileName, char* pEntryPoint);
+	void CreatePixelShader(__out ID3D11PixelShader** ppPS, char* pFileName, char* pEntryPoint);
 
 protected:
-	enum ED3DCounter
-	{
-		CTVertexBuffer = 1,
-		CTRenderTarget = 1,
-	};
-
-	D3DGraphic() {}
+	D3DGraphic();
 	D3DGraphic(const D3DGraphic&) {}
 	~D3DGraphic() {}
+
+	inline void RecreateBackBuffer()
+	{
+		ID3D11Texture2D* pBackBuffer = nullptr;
+		HRCheck(m_SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer));
+		HRCheck(m_D3DDevice->CreateRenderTargetView(pBackBuffer, nullptr, m_DefaultRenderTarget.GetResource()));
+		SafeRelease(pBackBuffer);
+	}
+
+	void CompileShaderFile(__out ID3DBlob** ppRes, char* pFileName, char* pEntryPoint, char* pTarget);
 private:
 	static D3DGraphic* m_sInstance;
+
+	struct D3DIndexBuffer
+	{
+		ID3D11Buffer* IndexBuffers;
+		DXGI_FORMAT   Format;
+		uint32_t      Offset;
+	};
+
+	struct D3DVertexBuffer
+	{
+		ID3D11Buffer* VertexBuffers[eCTVertexStream];
+		uint32_t      Stride[eCTVertexStream];
+		uint32_t      Offset[eCTVertexStream];
+	};
+
+	struct D3DPipelineState
+	{
+		ID3D11InputLayout*       InputLayout;
+		D3DVertexBuffer          VertexBuffer;
+		D3DIndexBuffer           IndexBuffer;
+		D3D_PRIMITIVE_TOPOLOGY   PrimitiveTopology;
+		ID3D11VertexShader*      VertexShader;
+		ID3D11RasterizerState*   RasterizerState;
+		D3D11_VIEWPORT*          Viewports;
+		ID3D11PixelShader*       PixelShader;
+		ID3D11BlendState*        BlendState;
+		ID3D11DepthStencilState* DepthStencilState;
+		ID3D11RenderTargetView*  RenderTargetView;
+		ID3D11DepthStencilView*  DepthStencilView;
+
+		float                    BlendFactor[4];
+		uint32_t                 SampleMask;
+	}m_D3DPipeLineState;
+
+	DXGI_SWAP_CHAIN_DESC m_SwapChainDesc;
 
 	Ref<IDXGISwapChain> m_SwapChain;
 	Ref<ID3D11Device> m_D3DDevice;
 	Ref<ID3D11DeviceContext> m_D3DContext;
-	Ref<ID3D11RenderTargetView> m_BackBuffer;
-	Ref<ID3D11DepthStencilView> m_DepthStencilView;
+	Ref<ID3D11RenderTargetView> m_DefaultRenderTarget;
+	Ref<ID3D11DepthStencilView> m_DefaultDepthStencilView;
+
+	bool m_FlushState[eFSCount];
 };
