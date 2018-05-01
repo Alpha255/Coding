7d965a48f2e543c5cfedcbe1205d6a8ec3b4ecbe
Alpha255 <189815810@qq.com>
2018-05-02 00:45:22 +0800

    Finish linear list

--- Fort/DataStructures/Node.h
+++ Fort/DataStructures/Node.h
@@ -1,10 +1,22 @@
 #pragma once
 
 #include <stdint.h>
+#include <assert.h>
+#include <memory>
 
-template<typename T> struct Node
+template<typename T> struct node
 {
 	T *Next = nullptr;
 	T *Prev = nullptr;
-	T Data;
+
+	T Data = 0;
 };
+
+template<typename T> inline void freeMemory(T *pMemory)
+{
+	if (pMemory)
+	{
+		delete[] pMemory;
+		pMemory = nullptr;
+	}
+}
