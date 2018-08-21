6ca9161db73481a1daea67ba5ec19c500031d81c
MaXiao <189815810@qq.com>
2018-08-21 18:43:41 +0800

    +* Start to Vulkan sample

---
+++ Vulkan/VulkanEngine.cpp
@@ -0,0 +1,8 @@
+#include "VulkanEngine.h"
+
+#include <vulkan/vulkan.h>
+
+void VulkanEngine::Initialize(HWND hWnd, uint32_t width, uint32_t height, bool bWindowed)
+{
+	///vkEnumeratePhysicalDevices();
+}
---
+++ Vulkan/VulkanEngine.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Common.h"
+
+class VulkanEngine : public NoneCopyable
+{
+public:
+	static VulkanEngine &Instance()
+	{
+		if (!s_Instance)
+		{
+			s_Instance = std::unique_ptr<VulkanEngine, std::function<void(VulkanEngine *)>>
+				(new VulkanEngine(), [](VulkanEngine *pEngine) { SafeDelete(pEngine) });
+		}
+		return *s_Instance;
+	}
+
+	inline void Flush()
+	{
+	}
+
+	inline void SetVSync(bool bVSync)
+	{
+	}
+
+	inline void SetFullScreen(bool bFullScreen)
+	{
+	}
+
+	inline void Draw(uint32_t vertexCount, uint32_t startVertex, uint32_t primitive)
+	{
+	}
+
+	inline void DrawIndexed(uint32_t indexCount, uint32_t startIndex, int32_t offset, uint32_t primitive)
+	{
+	}
+
+	void Initialize(HWND hWnd, uint32_t width, uint32_t height, bool bWindowed);
+
+protected:
+	VulkanEngine() = default;
+	~VulkanEngine() = default;
+
+private:
+	static std::unique_ptr<VulkanEngine, std::function<void(VulkanEngine *)>> s_Instance;
+
+	bool m_Inited = false;
+};
