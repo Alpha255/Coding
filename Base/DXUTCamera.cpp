62a4add15fc8ef3e1ecedc72da430ef2260917db
alpha255 <189815810@qq.com>
2019-05-28 00:44:55 +0800

    + (WIP) Add DXUTCamera

--- Base/DXUTCamera.cpp
+++ Base/DXUTCamera.cpp
@@ -1,51 +1,99 @@
 #include "DXUTCamera.h"
 
+DXUTCamera::DXUTCamera()
+{
+	SetViewParams(Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, 1.0f));
+	SetProjParams(DirectX::XM_PIDIV4, 1.0f, 1.0f, 1000.0f);
+
+	::GetCursorPos((::LPPOINT)&m_MouseAction.LastPosition);
+}
+
+void DXUTCamera::HandleWindowMessage(uint32_t msg, ::WPARAM wParam, ::LPARAM)
+{
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+	{
+		eKeyAction action = GetKeyAction(wParam);
+		if (eUnknown != action)
+		{
+
+		}
+	} 
+		break;
+	case WM_KEYUP:
+		break;
+	case WM_RBUTTONDOWN:
+	case WM_MBUTTONDOWN:
+	case WM_LBUTTONDOWN:
+	case WM_RBUTTONDBLCLK:
+	case WM_MBUTTONDBLCLK:
+	case WM_LBUTTONDBLCLK:
+		break;
+	case WM_RBUTTONUP:
+	case WM_MBUTTONUP:
+	case WM_LBUTTONUP:
+		break;
+	case WM_CAPTURECHANGED:
+		break; 
+	case WM_MOUSEWHEEL:
+		m_MouseAction.WheelDelta += (int32_t)HIWORD(wParam);
+		break;
+	}
+}
+
+void DXUTCamera::SetViewParams(const Vec3 &eye, const Vec3 &lookAt)
+{
+	m_DefaultEye = m_Eye = eye;
+	m_DefaultLookAt = m_LookAt = lookAt;
+
+	Vec3 up(0.0f, 1.0f, 0.0f);
+	m_View = Matrix::LookAtLH(m_Eye, m_LookAt, up);
+
+	Matrix inverseView = Matrix::Inverse(m_View);
+
+	Vec3 zBasis(inverseView.m[2]);
+	m_Yaw = atan2f(zBasis.x, zBasis.z);
+	float len = sqrtf(zBasis.z * zBasis.z + zBasis.x * zBasis.x);
+	m_Pitch = -atan2f(zBasis.y, len);
+}
+
+void DXUTCamera::SetProjParams(float fov, float aspect, float nearPlane, float farPlane)
+{
+	m_FOV = fov;
+	m_Aspect = aspect;
+	m_NearPlane = nearPlane;
+	m_FarPlane = farPlane;
+
+	m_Proj = Matrix::PerspectiveFovLH(fov, aspect, nearPlane, farPlane);
+}
+
 DXUTCamera::eKeyAction DXUTCamera::GetKeyAction(WPARAM wParam)
 {
 	switch (wParam)
 	{
-	case VK_CONTROL:
-		return eControlDown;
-	case VK_LEFT:
-		return eStrafeLeft;
-	case VK_RIGHT:
-		return eStrafeRight;
-	case VK_UP:
-		return eMoveForward;
-	case VK_DOWN:
-		return eMoveBackward;
-	case VK_PRIOR:
-		return eMoveUp;        /// page up
-	case VK_NEXT:
-		return eMoveDown;      /// page down
+	case VK_CONTROL: return eControlDown;
+	case VK_LEFT: return eStrafeLeft;
+	case VK_RIGHT: return eStrafeRight;
+	case VK_UP: return eMoveForward;
+	case VK_DOWN: return eMoveBackward;
+	case VK_PRIOR: return eMoveUp;       /// page up
+	case VK_NEXT: return eMoveDown;      /// page down
 
-	case 'A':
-		return eStrafeLeft;
-	case 'D':
-		return eStrafeRight;
-	case 'W':
-		return eMoveForward;
-	case 'S':
-		return eMoveBackward;
-	case 'Q':
-		return eMoveDown;
-	case 'E':
-		return eMoveUp;
+	case 'A': return eStrafeLeft;
+	case 'D': return eStrafeRight;
+	case 'W': return eMoveForward;
+	case 'S': return eMoveBackward;
+	case 'Q': return eMoveDown;
+	case 'E': return eMoveUp;
 
-	case VK_NUMPAD4:
-		return eStrafeLeft;
-	case VK_NUMPAD6:
-		return eStrafeRight;
-	case VK_NUMPAD8:
-		return eMoveForward;
-	case VK_NUMPAD2:
-		return eMoveBackward;
-	case VK_NUMPAD9:
-		return eMoveUp;
-	case VK_NUMPAD3:
-		return eMoveDown;
-	case VK_HOME:
-		return eReset;
+	case VK_NUMPAD4: return eStrafeLeft;
+	case VK_NUMPAD6: return eStrafeRight;
+	case VK_NUMPAD8: return eMoveForward;
+	case VK_NUMPAD2: return eMoveBackward;
+	case VK_NUMPAD9: return eMoveUp;
+	case VK_NUMPAD3: return eMoveDown;
+	case VK_HOME: return eReset;
 	}
 
 	return eUnknown;
--- Base/DXUTCamera.h
+++ Base/DXUTCamera.h
@@ -7,6 +7,10 @@ class DXUTCamera
 public:
 	DXUTCamera();
 	~DXUTCamera() = default;
+
+	void HandleWindowMessage(uint32_t msg, ::WPARAM wParam, ::LPARAM lParam);
+	void SetViewParams(const Vec3 &eye, const Vec3 &lookAt);
+	void SetProjParams(float fov, float aspect, float nearPlane, float farPlane);
 protected:
 	enum eKeyAction
 	{
@@ -29,7 +33,8 @@ protected:
 		bool RButtonDown = false;
 		int32_t WheelDelta = 0;
 		int32_t ButtonMask = 0;
-		std::pair<int32_t, int32_t> LastPosition = {};
+		int32_t LastPosition[2U] = {};
+		Vec2 Delta;
 	};
 
 	struct KeyboardAction
@@ -37,9 +42,29 @@ protected:
 		eKeyAction Action = eMaxKeys;
 		byte Keys[eMaxKeys] = {};
 		uint32_t KeysDown = 0;
+		Vec4 Direction;
 	};
 
 	eKeyAction GetKeyAction(WPARAM wParam);
+
+	void Reset();
 private:
 	MouseAction m_MouseAction;
+
+	Vec3 m_Eye;
+	Vec3 m_LookAt;
+
+	Vec3 m_DefaultEye;
+	Vec3 m_DefaultLookAt;
+
+	float m_FOV = 0.0f;
+	float m_Aspect = 1.0f;
+	float m_NearPlane = 0.0f;
+	float m_FarPlane = 0.0f;
+
+	float m_Yaw = 0.0f;
+	float m_Pitch = 0.0f;
+
+	Matrix m_View;
+	Matrix m_Proj;
 };
