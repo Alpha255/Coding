e1777e85fa286d0f43e4e157a6d6127eb665eae1
alpha255 <189815810@qq.com>
2019-09-18 00:26:18 +0800

    - Remove project generator

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 git push git@github.com:Alpha255/Coding.git
-rem git push git@bitbucket.org:alpha255/coding.git
-git push https://alpha255@bitbucket.org/alpha255/coding.git
-git push git@git.coding.net:alpha255/alpha255.git
 git push git@gitlab.com:Alpha255/Coding.git
-git push git@git.dev.tencent.com:alpha255/alpha255.git
+rem git push git@bitbucket.org:alpha255/coding.git
+rem git push https://alpha255@bitbucket.org/alpha255/coding.git
+rem git push git@git.coding.net:alpha255/alpha255.git
+rem git push git@git.dev.tencent.com:alpha255/alpha255.git
