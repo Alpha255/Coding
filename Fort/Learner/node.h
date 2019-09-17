c7d4dc2979a1440ba6be3408fe6fc984982c4517
alpha255 <189815810@qq.com>
2019-09-17 23:09:28 +0800

    * Learning...

--- Fort/Learner/node.h
+++ Fort/Learner/node.h
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <assert.h>
 #include <memory>
+#include <iostream>
+#include <stack>
+#include "Base/Base.h"
 
 template<typename T> struct node
 {
@@ -14,6 +17,25 @@ template<typename T> struct node
 
 	node *Prev = nullptr;
 	node *Next = nullptr;
+
+	T Data = 0;
+};
+
+template <typename T> struct treenode
+{
+	inline treenode() = default;
+	inline treenode(const T &value, treenode * pParent = nullptr, treenode *leftChild = nullptr, treenode *rightChild = nullptr)
+		: Data(value)
+		, Parent(pParent)
+		, LeftChild(leftChild)
+		, RightChild(rightChild)
+	{
+	}
+
+	treenode *Parent = nullptr;
+	treenode *LeftChild = nullptr;
+	treenode *RightChild = nullptr;
+	uint32_t Level = UINT32_MAX;
 
 	T Data = 0;
 };
