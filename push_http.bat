e1b86874d31606c327a2d3b4f16803f5d9aa7cc1
alpha255 <189815810@qq.com>
2020-03-30 00:10:50 +0800

    * Add back coding.net

--- push_http.bat
+++ push_http.bat
@@ -1,5 +1,6 @@
 git push https://github.com/Alpha255/Coding.git
 git push https://gitlab.com/Alpha255/Coding.git
+git push https://e.coding.net/alpha255/alpha255.git
 rem git push git@bitbucket.org:alpha255/coding.git
 rem git push https://alpha255@bitbucket.org/alpha255/coding.git
-rem git push https://git.coding.net/alpha255/alpha255.git
+@pause
