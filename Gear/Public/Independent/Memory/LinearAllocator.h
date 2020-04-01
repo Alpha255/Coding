142387119008954edd99ed3405a06c4c94960a11
alpha255 <189815810@qq.com>
2020-04-02 01:10:41 +0800

    * Template is meaningless here.

--- Gear/Public/Independent/Memory/LinearAllocator.h
+++ Gear/Public/Independent/Memory/LinearAllocator.h
@@ -4,20 +4,30 @@
 
 namespaceStart(gear)
 
-template<size_t Alignment> class linearAllocator : public noneCopyable
+/// Maybe a IAllocator
+
+class linearAllocator : public noneCopyable
 {
 public:
+	enum eAlignment
+	{
+		eDefaultAlignment = sizeof(uint32_t)
+	};
+
 	struct memoryPack
 	{
 		size_t Size = 0ull;
 		byte8_t *Memory = nullptr;
 	};
 
-	explicit linearAllocator(size_t size)
-		: linearAllocator()
-		, m_Size(size)
+	explicit linearAllocator(size_t size, size_t alignment = eDefaultAlignment)
 	{
-		m_Memory = std::make_unique<byte8_t>(new byte8_t[align_to(size, m_Alignment)]());
+		size_t alignment = align_to(alignment, 2ull);
+		m_Alignment = alignment < eDefaultAlignment ? eDefaultAlignment : alignment;
+		assert(m_Current.is_lock_free());
+
+		m_Size = align_to(size, m_Alignment);
+		m_Memory = std::make_unique<byte8_t>(new byte8_t[m_Size]());
 		///assert(atomic_is_lock_free(m_Memory));
 	}
 
@@ -36,56 +46,44 @@ public:
 		return m_Size - allocatedSize();
 	}
 
+	inline size_t alignment() const
+	{
+		return m_Alignment;
+	}
+
 	static inline size_t align_to(size_t size, size_t alignment)
 	{
 		return size + size % alignment;
 	}
 
-	memoryPack alloc(size_t size, size_t alignment);
-
-	void dealloc(memoryPack &pack);
-
-	void resize(size_t size);
-
-private:
-	linearAllocator()
+	memoryPack alloc(size_t size, size_t alignment)
 	{
-		size_t alignment = align_to(Alignment, 2ull);
-		m_Alignment = alignment < c_DefaultAlignment ? c_DefaultAlignment : alignment;
-		assert(m_Current.is_lock_free());
+		return memoryPack();
 	}
 
+	void dealloc(memoryPack &pack)
+	{
+	}
+
+	void resize(size_t size)
+	{
+		if (size <= m_Size)
+		{
+			return;
+		}
+
+		size_t newSize = align_to(size, m_Alignment);
+		std::unique_ptr<byte8_t> newMemory = std::make_unique<byte8_t>(new byte8_t[newSize]());
+		::memcpy_s(reinterpret_cast<void*>(*newMemory), m_Size, reinterpret_cast<const void*>(*m_Memory), m_Size);
+		m_Size = newSize;
+		m_Memory.reset(newMemory.release());
+	}
+
+private:
 	size_t m_Size = 0u;
 	size_t m_Alignment = 0u;
 	std::unique_ptr<byte8_t> m_Memory = nullptr;
 	std::atomic<size_t> m_Current = 0ull;
-	constexpr size_t c_DefaultAlignment = sizeof(uint32_t);
 };
-
-template<size_t Alignment>
-linearAllocator<Alignment>::memoryPack linearAllocator<Alignment>::alloc(size_t size, size_t alignment)
-{
-	return nullptr;
-}
-
-template<size_t Alignment>
-void linearAllocator<Alignment>::dealloc(memoryPack &pack)
-{
-}
-
-template<size_t Alignment>
-void linearAllocator<Alignment>::resize(size_t size)
-{
-	if (size <= m_Size)
-	{
-		return;
-	}
-
-	size_t newSize = align_to(size, m_Alignment);
-	std::unique_ptr<byte8_t> newMemory = std::make_unique<byte8_t>(new byte8_t[newSize]());
-	::memcpy_s(*newMemory, m_Size, *m_Memory, m_Size);
-	m_Size = newSize;
-	m_Memory = newMemory.release();
-}
 
 namespaceEnd(gear)
--- push_http.bat
+++ push_http.bat
@@ -1,6 +1,4 @@
 git push https://github.com/Alpha255/Coding.git
 git push https://gitlab.com/Alpha255/Coding.git
 git push https://e.coding.net/alpha255/alpha255.git
-rem git push git@bitbucket.org:alpha255/coding.git
-rem git push https://alpha255@bitbucket.org/alpha255/coding.git
 @pause
