912ab754c324859923f2756854dc817c7e810015
maxiao <maxiao@blackjack-inc.com>
2020-04-17 13:28:43 +0800

    * Reading stl sourcecode

--- Fort/Learner/FakeSTL/memory.hpp
+++ Fort/Learner/FakeSTL/memory.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#define DefaultAlignment 16ull
+
 template<class T> struct RemoveReference
 {
 	using Type = T;
@@ -23,9 +25,52 @@ template<class T> constexpr T &&Forward(RemoveReference<T> &&arg) noexcept
 	return static_cast<T &&>(arg);
 }
 
+template<class T> constexpr const T &Min(const T &left, const T &right)
+{
+	return right < left ? right : left;
+}
+
+template<class T> constexpr const T &Max(const T &left, const T &right)
+{
+	return left < right ? right : left;
+}
+
+template<class T> constexpr size_t Align = Max(alignof(T), static_cast<size_t>(DefaultAlignment));
+
 template<class T> class Allocator
 {
 public:
+	const_cast Allocator() noexcept {}
+	constexpr Allocator(const Allocator &) noexcept = default;
+
+	inline void Deallocate(T *const ptr, const size_t count)
+	{
+		/// Adjust???
+		::operator delete(ptr, sizeof(T) * count;
+	}
+
+	T *Allocate(const size_t count)
+	{
+		constexpr size_t maxCount = static_cast<size_t>(-1) / sizeof(T);
+		if (count < maxCount)
+		{
+			return ::operator new(sizeof(T) * count);
+		}
+		return nullptr;
+	}
+
+	template<class T, class... Args>
+	void construct(T *const ptr, Args &&... args)
+	{
+		::new(const_cast<void *>(static_cast<const volatile void *>(ptr)))
+			T(Forward<Args>(args)...);
+	}
+
+	template<class T>
+	void destory(T *const ptr)
+	{
+		ptr->~T();
+	}
 protected:
 private:
 };
--- Fort/Learner/FakeSTL/vector.hpp
+++ Fort/Learner/FakeSTL/vector.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
 #include "memory.hpp"
+#include <vector>
 
-template<class T> class Vector
+template<class T, class Alloc = Allocator<T>> 
+class Vector
 {
+public:
+	explicit Vector(const size_t count)
+	{
 
+	}
+protected:
+private:
 };
