ddef3d484c81f22b227fc0cfa3defdbad987e244
maxiao <maxiao@blackjack-inc.com>
2020-09-11 18:49:28 +0800

    * Large refactor-Gear-Add back (WIP)

---
+++ Gear/Gear.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Gear/Math/Math.h"
+#include "Gear/File.hpp"
+#include "Gear/CPUTimer.h"
+#include "Gear/Async/TaskScheduler.h"
+#include "Gear/System.h"
+#include "Gear/Camera/Camera.h"
+#include "Gear/Memory/LinearAllocator.h"
+
+using namespace Gear;
+using namespace Gear::Math;
