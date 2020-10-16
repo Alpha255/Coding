cb0ca55c88a6cb8c5a37b02004f40587137f1126
alpha255 <189815810@qq.com>
2020-10-17 00:56:14 +0800

    * Use dynamic library for renderers- TextureLoader

--- Colorful/Public/AssetTool/TextureLoader.hpp
+++ Colorful/Public/AssetTool/TextureLoader.hpp
@@ -15,7 +15,8 @@ enum class ETextureFormat : uint8_t
 	PSD,
 	GIF,
 	HDR,
-	PIC
+	PIC,
+	PNM
 };
 
 struct TextureFormatAttribute
@@ -60,6 +61,11 @@ public:
 protected:
 	static TextureDesc load_General(const std::string& path)
 	{
+		int32_t width = 0u;
+		int32_t height = 0u;
+		int32_t channels = 0u;
+		auto pixels = stbi_load(path.c_str(), &width, &height, &channels, STBI_rgb_alpha);
+
 		return TextureDesc();
 	}
 
@@ -89,6 +95,7 @@ std::vector<TextureFormatAttribute> TextureLoader::s_SupportedFormats =
 	{ ETextureFormat::GIF,  ".gif", "Graphics Interchange Format" },
 	{ ETextureFormat::HDR,  ".hdr", "High Dynamic Range" },
 	{ ETextureFormat::PIC,  ".pic", "QuickTime Picture?" },
+	{ ETextureFormat::PNM,  ".pgm", "Portable Anymap Format" }  /// Or .ppm
 };
 
 NAMESPACE_END(Gfx)
