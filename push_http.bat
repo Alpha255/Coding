5ad936a3dc5f059dd08295abdd32dbc61e8d378c
maxiao <maxiao@blackjack-inc.com>
2020-12-02 11:43:32 +0800

    * Disable HEXO

--- push_http.bat
+++ push_http.bat
@@ -1,7 +1,7 @@
 git push https://github.com/Alpha255/Coding.git
 git push https://gitlab.com/Alpha255/Coding.git
 git push https://e.coding.net/alpha255/alpha255.git
-cd Hexo
-git push https://github.com/Alpha255/Hexo.git
+rem cd Hexo
+rem git push https://github.com/Alpha255/Hexo.git
 cd ..
 @pause
